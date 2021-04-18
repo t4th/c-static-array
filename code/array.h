39d77f070ee190c4e50c816ae47e32ca6cbde29f
tath <tath@o2.pl>
2021-04-18 13:03:42 +0200

    Alternative implementation.

--- code/array.h
+++ code/array.h
@@ -6,48 +6,32 @@
     #define array_assert( x)
 #endif
 
-struct array_header
-{
-    const size_t m_length;
-    const size_t m_type_size;
-    const size_t m_size_in_bytes;
-} array_header;
+#define array_declare( type, length, name) \
+    typedef struct                                                  \
+    {                                                               \
+        type m_data[ length];                                       \
+    } name;                                                         \
+                                                                    \
+    type * name##_array_at( name * ap_data, size_t a_index)         \
+    {                                                               \
+        if ( a_index < length)                                      \
+        {                                                           \
+            return &ap_data->m_data[ a_index];                      \
+        }                                                           \
+        else                                                        \
+        {                                                           \
+            array_assert( !!0);                                     \
+            return &ap_data->m_data[ 0];                            \
+        }                                                           \
+    }                                                               \
 
-// Generic type function.
-void* array_dereference( const struct array_header a_header, void * const ap_data, const size_t a_index)
-{
-    // Cast void* to uint8_t* for defined -byte sized- pointer arithmetic.
-    const size_t byte_index = a_header.m_type_size * a_index;
+#define array( type, length, name) \
+    name##_data_type name##_data;                   \
 
-    if ( byte_index < a_header.m_size_in_bytes)
-    {
-        const uint8_t * const p_output = ( uint8_t *) ap_data;
-        return ( void*) ( &p_output[ byte_index]);
-    }
-    else
-    {
-        array_assert( !!0);
-    }
+#define array_at( name, index) name##_array_at( &name, index)
 
-    // I case of invalid index, return pointer to first element. Better than UB.
-    return ap_data;
-}
+#define array_length( name)
+#define array_len( name)
 
-#define array( type, length, name)                   \
-    typedef struct { type m_type; } name##_type;     \
-    struct array_header name##_header =              \
-    {                                                \
-        .m_length = length,                          \
-        .m_type_size = sizeof( type),                \
-        .m_size_in_bytes = (length * sizeof( type))  \
-    };                                               \
-    name##_type name##_array_data[ length]           \
-
-#define array_length( name)     name##_header.m_length
-#define array_len( name)        array_length(name)
-
-#define array_size( name)     name##_header.m_size_in_bytes
-
-#define array_at( name, index) (( name##_type*)array_dereference( name##_header, name##_array_data, index))->m_type
-
-#define array_loop_in_range( name, iterator_name) size_t iterator_name = 0U; for (; iterator_name < name##_header.m_length; ++iterator_name)
+#define array_size( name)
+#define array_loop_in_range( name, iterator_name)
--- example/main.c
+++ example/main.c
@@ -11,45 +11,13 @@ void out_of_bound_action( int expr)
     printf( "out of bound access occurred\n");
 }
 
+array_declare( float, 10, my_array_t);
+
 int main(void)
 {
-    array( float, 10, my_array) = { [2] = 1.23f};
+    my_array_t my_array = { .m_data = { [0] = 0.1f }};
 
-    printf( "array size = %zu bytes\n", array_size( my_array));
-
-    float read0 = array_at( my_array, 2);
-
-    array_at( my_array, 5) = 5.23f;
-
-    float read1 = array_at( my_array, 5);
-
-    array_loop_in_range( my_array, i)
-    {
-        printf( "%zu = %lf\n", i, array_at( my_array, i));
-    }
-
-    // out of bound read
-    float read2 = array_at( my_array, 10);
-    
-    // out of bound write
-    array_at( my_array, 15) = 56.123f;
-
-    struct my_struct
-    {
-        int a;
-        int b;
-        int c;
-    };
-    
-    // Initialize only element 1
-    array( struct my_struct, 10, my_struct_array) = {[1] = (struct my_struct){ .a = 1, .b = 2, .c = 3}};
-
-    printf( "my_struct_array.a = %d\n", array_at( my_struct_array, 1).a);
-
-    // out of bound read - read from element [0] instead
-    printf( "my_struct_array.b = %d\n", array_at( my_struct_array, 10).b);
-
-    printf( "my_struct_array.c = %d\n", array_at( my_struct_array, 1).c);
+    *my_array_t_array_at( &my_array, 1) = 3.2f;
 
     return 0;
 }
